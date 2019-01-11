e147e66f05eaeeaee16ebb5ef089d9fdbca1f3a4
dhrn <dharan19011996@gmail.com>
2019-01-11 20:42:33 +0530

    [docs] - How to use goverance extension

--- src/app/extensions.module.ts
+++ src/app/extensions.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
+import { AdfGovernanceModule, GovernanceExtensionService } from 'adf-governance';
+import { ContentManagementService } from './services/content-management.service';
 
 // Main entry point for external extensions only.
 // For any application-specific code use CoreExtensionsModule instead.
 
 @NgModule({
-  imports: []
+  imports: [
+    AdfGovernanceModule
+  ],
+  providers: [
+    {
+      provide: GovernanceExtensionService,
+      useExisting: ContentManagementService
+    }
+  ]
 })
 export class AppExtensionsModule {}
